6fdf05bbc271957caf96ecf37ea1d28bd2191edf
Martin W <martas@imm.cz>
2015-07-18 08:36:13 +0200

    Added mimetypes

---
+++ unity/mimetypes.py
@@ -0,0 +1,18 @@
+DASH_MIME = "application/dash+xml"
+DASH_MIME = "application/xml"
+ERROR_MSG_MIME = "text/plain"
+
+MEDIA_MIMES = {
+    ".mp4" : "video/mp4",
+    ".m4v" : "video/mp4",
+    ".m4a" : "audio/mp4"
+}
+
+SITE_MIMES = {
+    ".html" : "text/html",
+    ".css"  : "text/css",
+    ".js"   : "application/javascript",
+    ".ico"  : "image/x-icon", 
+    ".jpg"  : "image/jpeg",
+    ".png"  : "image/png"
+}
