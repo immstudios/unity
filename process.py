6afc58e7b84bf5aa1fd1cdfb0e5c3373ec16e0ff
Martastain <martas@imm.cz>
2015-08-18 14:13:34 +0200

    Encoder moved to packager class

--- process.py
+++ process.py
@@ -2,6 +2,7 @@
 
 import os
 import sys
+import json
 import time
 import subprocess
 
@@ -9,21 +10,9 @@ import subprocess
 from profiles import PROFILES
 
 
-try:
-    config = json.load(open("local_settings.json"))
-except:
-    config = {}
-
-
-
-
-
-PACKAGER  = "bin/packager"
-INPUT     = "input"  # Source directory
-INTER     = "inter"
-OUTPUT    = "output"   # Target directory
-
-  
+#
+# TODO: Use nxtools
+# 
 
 def ffmpeg(fin, fout, profile):
     cmd = [
@@ -38,6 +27,18 @@ def ffmpeg(fin, fout, profile):
     proc = subprocess.Popen(cmd)
     while proc.poll() == None:
         time.sleep(.1)
+
+
+def get_files(path):
+    for fname in os.listdir(path):
+        yield os.path.join(path, fname)
+
+
+
+#
+# Packing
+#
+
 
 
 
@@ -92,29 +93,7 @@ def packager_mp4box(fin, fout, repr_id):
 
 
 
-
-
-def encode_all():
-    for fname in os.listdir(INPUT):
-        bname = os.path.splitext(fname)[0]
-        fin = os.path.join(INPUT, fname)
-        foutpath = os.path.join(INTER, os.path.splitext(fname)[0])
-        try:
-            os.mkdir(foutpath)
-        except:
-            pass
-
-        for pname in PROFILES:
-            if pname.startswith("v"):
-                ext = ".m4v"
-            else:
-                ext = ".m4a"
-            fout = os.path.join(foutpath, pname + ext) 
-
-            if not os.path.exists(fout):
-                ffmpeg(fin, fout, PROFILES[pname])
-
-
+       
 def pack_all():
     for bname in os.listdir(INTER):
         finpath = os.path.join(INTER, bname)
@@ -129,8 +108,9 @@ def pack_all():
             repr_id = os.path.splitext(fname)[0]
             
             #print ("outpath", foutpath)
-            #packager_mp4box(fin, foutpath, repr_id)
-            packager_edash(fin, foutpath, repr_id)
+            packager_mp4box(fin, foutpath, repr_id)
+            #packager_edash(fin, foutpath, repr_id)
+            
 
 
 def check_nums():
@@ -153,17 +133,41 @@ def check_nums():
 
 
 
-class DashProcessor()
-    def __init__(self, **kwargs)
+class DashPackager():
+    def __init__(self, **kwargs):
         self.config = {
-                "source_path" : "input",
-                "inter_path" : "inter",
-                "target_path" : "data",
-
                 "segment_duration" : "2000"
-                
                 }
         self.config.update(kwargs)
+
+
+
+    def encode(self, fpath, output_path, profiles):
+        fname = os.path.basename(fpath)
+        bname = os.path.splitext(fname)[0]
+        output_path = os.path.join(output_path, bname)
+
+        try:
+            os.mkdir(output_path)
+        except:
+            pass
+
+        result = True
+
+        for pname in profiles:
+            if pname.startswith("v"):
+                ext = ".m4v"
+            else:
+                ext = ".m4a"
+            fout = os.path.join(output_path, pname + ext) 
+
+            if not os.path.exists(fout):
+                r = ffmpeg(fpath, fout, profiles[pname])
+                result = result and r
+
+        return result
+
+
 
 
     
@@ -171,6 +175,21 @@ class DashProcessor()
 
 
 if __name__ == "__main__":
-#    encode_all()
-#    pack_all()
-    check_nums()
+
+    try:
+        config = json.load(open("local_settings.json"))
+    except:
+        config = {}
+
+
+
+    source_dir  = config.get("media_input", "input")
+    temp_dir    = config.get("media_encoded", "inter")
+    output_dir  = config.get("media_dir", "output")
+
+   
+    packager = DashPackager()
+    for fpath in get_files(source_dir):
+        packager.encode(fpath, temp_dir, PROFILES)
+
+
