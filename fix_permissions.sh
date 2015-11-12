5cd4a7357ed32d2a450081eb0cd3dae72579b931
Martastain <martas@imm.cz>
2015-11-12 13:18:37 +0100

    fixing site directory

--- fix_permissions.sh
+++ fix_permissions.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
 BASEDIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
-chmod 755 unity
+find $BASEDIR/site/ -type d -exec chmod 775 {} +
+find $BASEDIR/site/ -type f -exec chmod 664 {} +
+
+chmod 755 site/
+chmod 755 unity/
+
 chmod 644 unity/*.py
 
 chmod 755 vendor/
 chmod 755 vendor.sh
 chmod 644 vendor.lst
-
-
 
 chmod 755 server.py
 chmod 644 .gitignore
