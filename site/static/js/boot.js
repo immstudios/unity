26bc6b571a0f4827b1ec5b60a50c196ecf98b68a
Martastain <martas@imm.cz>
2015-11-16 11:12:36 +0100

    added boot.js

---
+++ site/static/js/boot.js
@@ -0,0 +1,64 @@
+
+
+
+
+function start_stream(){
+
+    var manifest_url = '/manifest/' + session_id;
+    var fallback_video = '/static/video/unsupported.mp4';
+
+    var player = document.getElementById('player');
+    
+
+    if(Hls.isSupported()) {
+
+        var hls = new Hls();
+        hls.loadSource(manifest_url);
+        hls.attachVideo(player);
+        hls.on(Hls.Events.MANIFEST_PARSED, function() {
+                player.play();
+        });
+
+
+     } else {
+        // If HLS is not supported, play fallback video instead.
+        player.src = fallback_video;
+        player.load();
+
+        if (typeof player.loop == 'boolean') { 
+            // loop supported
+            player.loop = true;
+
+        } else { 
+            // loop property not supported
+            player.addEventListener('ended', function () {
+                this.currentTime = 0;
+                this.play();
+            }, false);
+        }
+        player.play();
+    
+     } // HLS is not supported
+ 
+
+    player.muted = true;
+
+} // function start_stream()
+
+
+
+
+
+
+
+
+$(document).ready(function() {
+    if (session_id == "False") {} 
+    else {
+        start_stream();
+    }
+
+});
+
+
+
