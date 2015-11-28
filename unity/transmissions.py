4833d68e7fb809551cd3b89c919aca7cd7edd89c
Martastain <martas@imm.cz>
2015-11-28 14:43:35 +0100

    added last_access time

--- unity/transmissions.py
+++ unity/transmissions.py
@@ -2,10 +2,13 @@ from .playlist import Playlist
 
 
 class Transmission():
-    def __init__(self, id_user, **kwargs):
+    def __init__(self, parent, id_user, **kwargs):
         self.id_user = id_user
         self.kwargs = kwargs
+        self.accessed()
 
+    def accessed(self):
+        self.last_access = time.time()
 
     def media(self, segment_number):
         pass
@@ -21,12 +24,28 @@ class Transmissions():
         self.parent = parent
         self.data = {}
 
+
     @property
     def settings(self):
         return self.parent.settings
 
+    def keys():
+        self.data.keys()
+
+    def create(self, id_user):
+        if not id_user in self.data:
+            self.data[id_user] = Transmission(self, id_user)
+    
+    def clean_up(self):
+        max_age = 3600
+        for id_user in self.data:
+            transmission = self.data[id_user] # Cannot use self.__getitem__() : we don't want to update last_access
+            if time.time() - transmission.last_access > max_age:
+                del(self.data[id_user])
+
     def __getitem__(self, key):
         if key in self.data:
+            self.data[key].accessed()
             return self.data[key]
         return False
 
