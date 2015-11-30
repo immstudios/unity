bbbe5d896b9ef210705f88c1ba0a12a57113dfc7
Martastain <martas@imm.cz>
2015-11-30 16:05:16 +0100

    Site update

--- site/static/js/boot.js
+++ site/static/js/boot.js
@@ -1,65 +1,7 @@
 
-
-
-
-function start_stream(){
-
-    var manifest_url = '/manifest/' + session_id;
-    var fallback_video = '/static/video/unsupported.mp4';
-
-    var player = document.getElementById('player');
-    
-
-    if(Hls.isSupported()) {
-
-        var hls = new Hls();
-        hls.loadSource(manifest_url);
-        hls.attachVideo(player);
-        hls.on(Hls.Events.MANIFEST_PARSED, function() {
-                player.play();
-        });
-
-
-     } else {
-        // If HLS is not supported, play fallback video instead.
-        player.src = fallback_video;
-        player.load();
-
-        if (typeof player.loop == 'boolean') { 
-            // loop supported
-            player.loop = true;
-
-        } else { 
-            // loop property not supported
-            player.addEventListener('ended', function () {
-                this.currentTime = 0;
-                this.play();
-            }, false);
-        }
-        player.play();
-    
-     } // HLS is not supported
- 
-
-    player.muted = true;
-
-} // function start_stream()
-
-
-
-
-
-
-
-
 $(document).ready(function() {
-
     jQuery(".status").fadeOut();
-    jQuery(".preloader").delay(1000).fadeOut("slow");
-
-
-
-
+    jQuery(".preloader").delay(500).fadeOut("slow");
 });
 
 
