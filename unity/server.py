dca4cbf2635338e2aefe8001a2be676df2a4739c
Martastain <martas@imm.cz>
2015-12-04 07:35:49 +0100

    Added video requests

--- unity/server.py
+++ unity/server.py
@@ -1,3 +1,4 @@
+import os
 import cherrypy
 import jinja2
 
@@ -7,7 +8,7 @@ from .transmissions import Transmissions
 manifest_headers = [        
         ["Content-Type", "application/vnd.apple.mpegurl"],
         ["Connection", "keep-alive"],
-        ["Cache-Control" "no-cache"],
+        ["Cache-Control", "no-cache"],
         ["Access-Control-Allow-Origin", "*"],
         ["Accept-Ranges", "bytes"]
     ]
@@ -16,31 +17,32 @@ class UnityServer():
     def __init__(self, **kwargs):
         self.settings = kwargs
         self.transmissions = Transmissions(self)
-
         template_root = self.settings.get("template_root", "site/templates")
         self.jinja = jinja2.Environment(
                 loader=jinja2.FileSystemLoader(template_root)
                 )
+
+    ##
+    # SITE REQUESTS
+    ##
 
 
     @cherrypy.expose
     def index(self):
         id_user = cherrypy.session.get("id_user")
         logging.debug("User ID {} requested index page".format(id_user))
-
         if not id_user:
             tpl = self.jinja.get_template("login.html")
         else:
             tpl = self.jinja.get_template("player.html")
-
         context = {}
         return tpl.render(**context)
+
 
     @cherrypy.expose
     def default(self, attr=""):
         tpl = self.jinja.get_template("error.html")
         return tpl.render(error_number=404, error_message="Nic takovyho tu neni")
-
 
 
     @cherrypy.expose
@@ -60,38 +62,34 @@ class UnityServer():
         raise cherrypy.HTTPRedirect("/")
         return "logged out. <a href=\"/\">continue</a>"
 
-    #
+    ##
     #  VIDEO REQUESTS
-    #    
+    ##    
 
     @cherrypy.expose
-    def manifest(self):
+    def manifest(self, variant=False):
         id_user = cherrypy.session["id_user"]
-        transmission = self.transmission(id_user)
+        transmission = self.transmissions[id_user]
         if not transmission:
             raise cherrypy.HTTPError(403, "Unauthorized")
 
-        #TODO: starting segment from session (and to session)
-
-        manifest = transmission.manifest()
-        for header, value in manifest_headers:
-            cherrypy.response.headers[header] = value
+        manifest = transmission.manifest(variant)
+        for h, v in manifest_headers:
+            cherrypy.response.headers[h] = v
         return manifest
 
 
-
     @cherrypy.expose
-    def media(self, segment):
-        #bname = os.path.splitext(segment)[0]
-        #try:
-        #    playlist_id, number = bname.split("-")
-        #except:
-        #    raise cherrypy.HTTPError(400, "Bad request")
-        
-        #media = self.playlist.file_at_segment(int(number))
-        #logging.debug("{} -> {}".format(number, media))a
-        media_root = "http://media.nxtv.cz/"
-        media_path = "????"
-        raise cherrypy.HTTPRedirect(media_root + media_path)
+    def media(self, fname):
+        id_user = cherrypy.session["id_user"]
+        transmission = self.transmissions[id_user]
+        bname = os.path.splitext(fname)[0]
+        try:
+            variant, segment = bname.split("-")
+            segment = int(segment)
+        except:
+            raise cherrypy.HTTPError(400, "Bad request")
+        media_path = transmission.media(variant, segment)
+        raise cherrypy.HTTPRedirect(media_path)
 
 
